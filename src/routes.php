ce93db35f267f307ccd86e52ef708fb535a0c590
willian.santos <willian1948@hotmail.com>
2023-03-06 14:07:58 -0300

    Adicionadas as rotas para adicionar o usuario e para autenticar

--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Controllers\AuthenticationController;
 use App\Controllers\CategoryController;
+use App\Controllers\UserController;
 use App\Controllers\VideoController;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
@@ -58,6 +60,16 @@ return function (RoutingConfigurator $routes) {
     $routes->add("get_videos_by_category", "/categories/{id}/videos")
         ->controller([CategoryController::class, "getVideosByCategory"])
         ->methods(["GET"])
+    ;
+
+    $routes->add("post_user", "/users")
+        ->controller([UserController::class, "store"])
+        ->methods(["POST"])
+    ;
+
+    $routes->add("post_authenticate", "/authenticate")
+        ->controller([AuthenticationController::class, "authenticate"])
+        ->methods(["POST"])
     ;
 
 };
