75af94834081a1b6e204eda622dd59634fe5d60f
willian.santos <willian1948@hotmail.com>
2023-03-06 14:00:13 -0300

    Criado service TokenParser para parsear o token em string de volta a Objeto Token

---
+++ src/Services/TokenParser.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Services;
+
+use Lcobucci\JWT\Configuration;
+use Lcobucci\JWT\Token;
+
+class TokenParser{
+    
+    public function __construct(
+        private Configuration $config
+    ){}
+
+    public function parse(string $jwt): Token
+    {
+        $token = $this->config->parser()->parse($jwt);
+
+        if (!$this->config->validator()->validate($token, ...$this->config->validationConstraints())) {
+            throw new \InvalidArgumentException('Invalid token provided');
+        }
+
+        return $token;
+    }
+
+}
